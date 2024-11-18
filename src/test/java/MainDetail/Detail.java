e7614008b314ccab319ddea4d7bcae8e151179a5
ThanhThanh <thanhqtksnb@gmail.com>
2024-11-19 00:24:56 +0700

    Share thông tin hàm

---
+++ src/test/java/MainDetail/Detail.java
@@ -0,0 +1,24 @@
+package MainDetail;
+
+public class Detail {
+
+    //Thông tin đăng nhập
+    public void inputInfoLogin(){
+
+    }
+    //Cập nhật thông tin
+    public void updateInfo(){
+
+    }
+    //Kiểm tra giá
+    public void checkPrice(){
+
+    }
+    //Kiểm tra sản phẩm
+    public void checkProduct(){
+
+    }
+
+
+
+}
