6fdbd335de6aa45559bb6c3cf4b14e5865fb3501
ThanhThanh <thanhqtksnb@gmail.com>
2024-11-20 23:54:43 +0700

    updateInfo

--- src/test/java/Main/Main.java
+++ src/test/java/Main/Main.java
@@ -5,9 +5,11 @@ import org.junit.Test;
 
 public class Main {
     @Test
-    public void case1(){
+    public void updateInfo() throws InterruptedException {
         Detail detail = new Detail();
         detail.openBrowser();
         detail.inputInfoLogin("0964639446","Thanhtran123");
+        detail.updateInfo();
+        detail.closeBrower();
     }
 }
--- src/test/java/MainDetail/Detail.java
+++ src/test/java/MainDetail/Detail.java
@@ -1,10 +1,14 @@
 package MainDetail;
 
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.time.Duration;
 
 public class Detail {
     WebDriver webDriver;
@@ -21,8 +25,21 @@ public class Detail {
     @FindBy(xpath = "//span[text()='Tài khoản']")
     private WebElement textAcc;
 
+    @FindBy(xpath = "(//span[@class='radiomark'])[1]")
+    private WebElement radiobuttonMale;
+    @FindBy(xpath = "(//span[@class='radiomark'])[2]")
+    private WebElement radiobuttonFeMale;
+    @FindBy(id ="btn-save-account-info")
+    private WebElement btnSave;
+    @FindBy(xpath = "//div[@class='my-account user-detail']//li[@class='success-msg']")
+    private WebElement textSaveSuccess;
+
+
+
+
     public void openBrowser() {
         webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         webDriver.manage().window().maximize();
         PageFactory.initElements(webDriver, this);
         webDriver.get(urlHome);
@@ -33,19 +50,71 @@ public class Detail {
         inputUser.sendKeys(username);
         inputPass.sendKeys(password);
         buttonLogin.click();
-
     }
+    /* Cập nhập thông tin cá nhân:
++ Đổi giới tính nếu đang là Nam thì sẽ chọn lại là Nữ và ngược lại.
++ Click "Lưu thay đổi"
++ Verify message thành công "Thông tin tài khoản đã được lưu lại."
++ Reload trang check xem data thực sự được cập nhật chưa
+*/
     //Cập nhật thông tin
-    public void updateInfo(){
+    public void updateInfo() throws InterruptedException {
+
+
+
+      /*  JavascriptExecutor js = (JavascriptExecutor) webDriver;
+        js.executeScript("window.scrollBy(0, 1000)");*/
+
+
+
+        // Cuộn đến phần tử đó
+        JavascriptExecutor js = (JavascriptExecutor) webDriver;
+        js.executeScript("arguments[0].scrollIntoView(true);", radiobuttonMale);
+       
+        // Đổi giới tính nếu đang là Nam thì sẽ chọn lại là Nữ và ngược lại.
+        if(radiobuttonMale.isSelected() && !radiobuttonFeMale.isSelected()){
+            System.out.println("Giới tính Nam đang được chọn , tui chọn lại giới tính Nữ nhaaa");
+            radiobuttonFeMale.click();
+        }else if(radiobuttonFeMale.isSelected() && !radiobuttonMale.isSelected()) {
+            System.out.println("Giới tính Nữ đang được chọn , tui chọn lại giới tính Nam nhaaa");
+            radiobuttonMale.click();
+        }else {
+            //???KHum hỉu saoo không chọn dc radiobutton
+            System.out.println("Trạng thái checkboxNam: " + radiobuttonMale.isSelected());
+            System.out.println("Trạng thái checkboxNữ: " + radiobuttonFeMale.isSelected());
+            System.out.println("Cả hai checkbox đều chưa được chọn.");
+        }
+        //Click "Lưu thay đổi"
+        btnSave.click();
+
+        //Verify message thành công "Thông tin tài khoản đã được lưu lại."
+        Assert.assertEquals("Thông tin khum có khớp","Thông tin tài khoản đã được lưu lại.",textSaveSuccess.getText());
+
+        // Reload lại trang sau khi lưu
+        webDriver.navigate().refresh();
+
+        //Kiểm tra phần tử radio button đã được chọn chưa
+
+        if(radiobuttonMale.isSelected()){
+            System.out.println("Đã chọn giời tính Nam");
+            Assert.assertTrue(radiobuttonMale.isSelected());
+        }else if(radiobuttonFeMale.isSelected()){
+            System.out.println("Đã chọn giới tính Nữ");
+            Assert.assertTrue(radiobuttonFeMale.isSelected());
+        }
+
 
     }
     //Kiểm tra giá
-    public void checkPrice(){
+    public void checkFilterPrice(){
 
     }
     //Kiểm tra sản phẩm
     public void checkProduct(){
 
+    }
+    public void closeBrower() {
+        webDriver.quit();
     }
 
 
